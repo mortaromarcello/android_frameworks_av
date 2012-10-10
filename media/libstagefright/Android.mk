65589388a1bad14a9109c9482bc1b004ce16a8d2
mortaromarcello <mortaromarcello@gmail.com>
2012-10-10 23:02:09 +0200

    modified:   media/libstagefright/Android.mk

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -131,6 +131,7 @@ LOCAL_STATIC_LIBRARIES := \
         libstagefright_aacenc \
         libstagefright_matroska \
         libstagefright_timedtext \
+        libstagefright_rtsp \
         libvpx \
         libstagefright_mpeg2ts \
         libstagefright_id3 \
